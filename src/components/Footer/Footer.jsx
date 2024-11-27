c1df9d94cdc55f7fd35cf38a15e6d5c4a0168b30
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-27 19:08:19 +0100

    FIX: components class container

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <>
             <footer className={style.pageFooter}>
-                <div className='container'>
+                <div className={style.container}>
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat eius ad exercitationem officiis id dicta minima sunt eligendi asperiores iusto impedit minus odio, doloribus assumenda saepe ipsum officia ut. Aperiam! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla reprehenderit ipsam et tempora ipsa. In pariatur aut dolor placeat exercitationem, dolore.</p>
                 </div>
             </footer>
