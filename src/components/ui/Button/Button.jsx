423841a907cd225ebe9abdbc88b617118f7c04f1
ChristianScarabelli <scachmabol@hotmail.it>
2024-11-27 17:18:42 +0100

    ADD: button & footer

--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -3,6 +3,7 @@ import style from './Button.module.css'
 export default function Button() {
     return (
         <>
+            <button className={style.button}>leggi di più</button>
         </>
     )
 }
